2f5815e4eabcba1c1f53995e2c1ca522e9e832a8
Ran <konankid10129@gmail.com>
2024-08-30 17:27:21 +0800

    update code

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const num = ref(2);
+function clickMe() {
+  num.value++;
+}
+</script>
+
+<template>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">點我</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
